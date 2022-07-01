ab0dc8d7d9f8d8945a9721fb1e4272ca61c72152
Randall <randalltr@proton.me>
2022-07-01 13:34:38 +0700

    created make_board function

--- board.py
+++ board.py
@@ -1,10 +1,15 @@
-cell = "| |"
-rows = 7
-cols = 6
-column = ""
-
-for i in range(cols):
-    for j in range(rows):
-        column += cell
-    print(column)
+def make_board():
+    cell = "| |"
+    rows = 7
+    cols = 6
     column = ""
+    print(" 1  2  3  4  5  6  7 ")
+    for i in range(cols):
+        for j in range(rows):
+            column += cell
+        print(column)
+        column = ""
+    print("---------------------")
+
+
+make_board()
